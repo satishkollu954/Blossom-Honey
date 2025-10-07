2f6991493e164a7d3dd07c27b0012a6f3bfbb0bc
SriKavyaTakuru23 <srikavya.takuru@raagvitech.com>
2025-10-07 15:55:38 +0530

    message

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import Home from './components/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+     
       <h1>Vite + React</h1>
+      <Home />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
