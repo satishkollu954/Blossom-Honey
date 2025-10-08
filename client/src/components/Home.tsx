1f362d0d2c7a755c754bfde548f72cbff0dd77f2
Satish Kollu <satishkollu954@gmail.com>
2025-10-08 16:56:56 +0530

    tailwind is imported

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div className="flex items-center justify-center h-screen">
-      <p className="text-4xl font-bold text-blue-600">Hello Kavya welcome to Tailwind</p>
+      <p className="text-4xl text-blue-600">Hello Kavya welcome to Tailwind</p>
     </div>
   )
 }
